6019dbe3a10872ee940322e41f7e541c5ee91450
atusiniida <niiyan@MacBook-Pro-6.local>
2018-05-23 13:51:05 +0900

    1

--- bwa.sh
+++ bwa.sh
@@ -7,8 +7,8 @@
 PREFIX=$1 # read10000
 REFFILE=$2 # ref/ecoli_index
 
-bwa-0.7.12/bwa mem  ${REFFILE}   ${PREFIX}.fastq > ${PREFIX}.sam
-#bwa-0.7.12/bwa mem  read10000   read10000.fastq > read10000.sam
+bwa mem  ${REFFILE}   ${PREFIX}.fastq > ${PREFIX}.sam
+#bwa mem  read10000   read10000.fastq > read10000.sam
 
-#bwa-0.7.12/bwa aln ${REFFILE} ${PREFIX}.fastq > ${PREFIX}.sai
-#bwa-0.7.12/bwa samse ${REFFILE} ${PREFIX}.sai ${PREFIX}.fastq > ${PREFIX}.sam
+#bwa aln ${REFFILE} ${PREFIX}.fastq > ${PREFIX}.sai
+#bwa samse ${REFFILE} ${PREFIX}.sai ${PREFIX}.fastq > ${PREFIX}.sam
